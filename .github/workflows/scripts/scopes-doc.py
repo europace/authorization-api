6add903b077ccd77d110e863562dbd122f47e4e5
Oliver Otzen <oliver.otzen@hypoport.de>
2020-06-26 17:45:32 +0200

    Kleine Rechtschreib- und Grammatik- Korrektur
    "Mithilfe" hat eine andere Bedeutung als "mit Hilfe". Rest ist Grammatik. Bin zwar kein Deutsch Genie, aber das klang schon sehr komisch.

--- .github/workflows/scripts/scopes-doc.py
+++ .github/workflows/scripts/scopes-doc.py
@@ -9,7 +9,7 @@ def transformScopesToMarkDown(input):
 
     intro="""# Scopes für OAuth Clients bei Europace
 
-Mithilfe von Scopes kannst du genau angeben, welche Art von Zugriff du benötigst. Scopes beschränken den Zugriff für OAuth-Token. Sie gewähren keine zusätzlichen Berechtigungen über das hinaus, was der Benutzer bereits hat.
+Mit Hilfe der Scopes kannst du genau angeben, welche Art von Zugriff du benötigst. Scopes beschränken den Zugriff mittels OAuth-Tokens. Sie gewähren keine zusätzlichen Berechtigungen über die Rechte des Nutzers hinaus.
 
 ## Verfügbare Scopes
 
